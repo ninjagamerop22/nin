62562fd95e59738cb796965b63a39ce7d63459a2
Ninja Gamerop <151740669+ninjagamerop22@users.noreply.github.com>
2024-10-28 15:54:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "REPO_NAME=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$REPO_NAME/NINJA.py",
+    "postStartCommand": "REPO_NAME=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$REPO_NAME/shit.py",
     "customizations": {
         "vscode": {
             "settings": {
